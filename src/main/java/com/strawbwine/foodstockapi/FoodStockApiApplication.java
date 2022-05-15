d8856f1462c748d6178042be35ee8329debbde09
StrawbWine <severin.hanevik@gmail.com>
2022-05-15 15:13:44 +0200

    🎉Initial API setup by Spring Boot Initializr

---
+++ src/main/java/com/strawbwine/foodstockapi/FoodStockApiApplication.java
@@ -0,0 +1,13 @@
+package com.strawbwine.foodstockapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodStockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/strawbwine/foodstockapi/FoodStockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strawbwine.foodstockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodStockApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
