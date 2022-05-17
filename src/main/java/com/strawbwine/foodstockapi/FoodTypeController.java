14c448889a633bcc1a642c7d6ec544c702847af0
StrawbWine <severin.hanevik@gmail.com>
2022-05-17 19:39:27 +0200

    🔧Add CORS for React app at localhost:3000

--- src/main/java/com/strawbwine/foodstockapi/FoodTypeController.java
+++ src/main/java/com/strawbwine/foodstockapi/FoodTypeController.java
@@ -1,9 +1,6 @@
 package com.strawbwine.foodstockapi;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,11 +12,13 @@ public class FoodTypeController {
         this.repository = repository;
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/foodtypes")
     List<FoodType> all() {
         return repository.findAll();
     }
 
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/foodtypes")
     FoodType newFoodType(@RequestBody FoodType newFoodType) {
         return repository.save(newFoodType);
